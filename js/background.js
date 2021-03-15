c95105611e3e8d391a2c9f2ccddd56b1513e853f
deoncarr <71791747+deoncarr@users.noreply.github.com>
2021-03-15 09:39:52 -0700

    Update background.js
    Add additional defaults for BCM

--- js/background.js
+++ js/background.js
@@ -19,18 +19,41 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.get(['defaultUrl', 'defaultRegex', 'projects'], function(result) {
     if (result.defaultUrl === undefined || result.defaultUrl === null) {
-      chrome.storage.sync.set({defaultUrl: 'https://issues.apache.org/jira/browse/'}, function() {
+      chrome.storage.sync.set({defaultUrl: 'https://broadcomcsm.service-now.com/sn_customerservice_case.do?sysparm_query=number='}, function() {
         console.log('Setting initial JIRA URL')
       });
     }
     if (result.defaultRegex === undefined || result.defaultRegex === null) {
-      chrome.storage.sync.set({defaultRegex: '[A-Z]{2,20}-\\d{1,7}'}, function() {
+      chrome.storage.sync.set({defaultRegex: '[C,c][S,s]\\d{6,12}'}, function() {
         console.log('Setting initial regex')
       });
     }
     if (result.projects === undefined || result.projects === null) {
-      chrome.storage.sync.set({projects: []}, function() {
-        console.log('Setting initial empty project list')
+      var projectsArray = [{
+        "url": "https://ingjira.broadcom.net/browse/",
+        "regex": "[S,s][D,d][K,k]-\\d{4,7}"
+      },{
+        "url": "https://broadcomcsm.service-now.com/csm?id=search&t=kb&q=",
+        "regex": "[K,k][B,b]\\d{6,8}"       
+      },{
+        "url": "https://ingjira.broadcom.net/browse/",
+        "regex": "[A,a][R,r][C,c][H,h][A,a][D,d]-\\d{1,5}"       
+      },{
+        "url": "https://ingjira.broadcom.net/browse/",
+        "regex": "[U,u][K,k][E,e][R,r][N,n][E,e][L,l]-\\d{1,5}"       
+      },{
+        "url": "https://ingjira.broadcom.net/browse/",
+        "regex": "[K,k][B,b][P,p][S,s][D,d][K,k]-\\d{1,5}"       
+      },{
+        "url": "https://ingjira.broadcom.net/browse/",
+        "regex": "[K,k][B,b][P,p]-\\d{1,5}"       
+      }
+    ]
+
+      chrome.storage.sync.set({
+        'projects': projectsArray
+      }, function() {
+        console.log('Saved project URLs ' + JSON.stringify(projectsArray));
       });
     }
   });
